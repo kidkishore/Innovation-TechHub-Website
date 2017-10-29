7965af43796fbf67cb0679f4715be3fc2aff95ca
KISHORE <adimula2@illinois.edu>
2017-10-29 12:29:05 -0500

    committing

---
+++ factorial.cs
@@ -0,0 +1,12 @@
+((factorial(13)/(factorial(2)*factorial(11)))*(factorial(4)/(factorial(2)*factorial(4)))*2.*(factorial(11)
+
+
+
+
+/(factorial(1)*factorial(10)))*(factorial(4)/
+
+
+(factorial(1)*factorial(3))))/
+
+
+((factorial(52)/(factorial(5)*factorial(47))))
